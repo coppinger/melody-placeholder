cb87979c4db192b68a092281ee1628f6d88a8ee9
coppinger <charles.coppinger@gmail.com>
2023-10-18 09:48:48 +1300

    bus stop

---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,7 @@
+<div
+    class="w-screen h-screen bg-orange-100 flex flex-col items-center justify-center gap-32 overflow-x-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]"
+>
+    <div class="flex flex-col gap-12 items-center text-center w-full">
+        <p>We're excited that you're excited</p>
+    </div>
+</div>
